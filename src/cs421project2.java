71b0d602857924229883ee6120bf95b3966ca090
Jared Powell <jared.powell261@topper.wku.edu>
2018-11-22 20:20:39 -0600

    Finished project to the best of my knowledge. Just documentation and debugging left to do

--- src/cs421project2.java
+++ src/cs421project2.java
@@ -4,7 +4,6 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.LinkedList;
 import java.util.HashMap;
-import java.util.ListIterator;
 
 public class cs421project2 {
 
@@ -13,18 +12,16 @@ public class cs421project2 {
     public static void main(String[] args) throws FileNotFoundException {
         LinkedList list;
 
-        File file = new File("words2.txt");
+        File file = new File("words.txt");
         Scanner in = new Scanner(file);
 
         String numberPad;
         while (in.hasNext()) {
             String next = in.next().toLowerCase();
-            if (next.length() != 3 && next.length() != 4 && next.length() != 6
-                    && next.length() != 7 && next.length() != 10) {
+            if (next.length() != 3 && next.length() != 4 && next.length() != 7 && next.length() != 10) {
                 continue;
             }
             numberPad = toNumberPad(next);
-            System.out.println(numberPad);
 
             if (map.get(numberPad) == null) {
                 LinkedList newList = new LinkedList();
@@ -35,12 +32,15 @@ public class cs421project2 {
                 list.add(next);
             }
         }
-        System.out.println(map.size());
         while (true) {
             in = new Scanner(System.in);
-            System.out.print("Enter a 10 digit phone number: ");
+            System.out.print("Enter a 10 digit phone number (or type exit to quit): ");
             String number = in.next();
             if (number.length() != 10) {
+                if(number.equalsIgnoreCase("exit")) {
+                    System.out.println("Good-bye!");
+                    System.exit(0);
+                }
                 System.out.println("Invalid phone number");
                 continue;
             }
@@ -106,7 +106,7 @@ public class cs421project2 {
     }
 
     private static void findWords(String number) {
-        LinkedList<String> list = new LinkedList<>();
+        LinkedList<String> list;
         /*
         1) If there is only word-based representation of whole 7 digits
         2) Else If there is only word-based representation of exchange (3 digit) and number (4 digit)
@@ -114,35 +114,52 @@ public class cs421project2 {
         4) Else If there is word-based representation of only number (4 digit)
         5) Else there is no word-based representation given number.
          */
+        if (!isOnlyNumbers(number)) {
+            return;
+        }
+
         if (map.containsKey(number)) {
             list = map.get(number);
-        }
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println("1-" + list.get(i).toUpperCase());
-        }
-        if (map.containsKey(number.substring(3))) {
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println("1-" + list.get(i).toUpperCase());
+            }
+        } else if (map.containsKey(number.substring(3))) {
             list = map.get(number.substring(3));
             for (int i = 0; i < list.size(); i++) {
                 System.out.println("1-" + number.substring(0, 3) + "-" + list.get(i).toUpperCase());
             }
+        } else if(map.containsKey(number.substring(3,6)) && map.containsKey(number.substring(6))) {
+            list = map.get(number.substring(3,6));
+            LinkedList<String> list2 = map.get(number.substring(6));
+            for(int i = 0; i < list.size(); i++) {
+                for(int j = 0; j < list2.size(); j++) {
+                    System.out.println("1-" + list.get(i).toUpperCase() + "-" + list2.get(j).toUpperCase());
+                }
+            }
+        } else if (map.containsKey(number.substring(3, 6))) {
+            list = map.get(number.substring(3, 6));
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println("1-" + number.substring(0, 3) + "-" + list.get(i).toUpperCase() + "-" + number.substring(6));
+            }
+        } else if (map.containsKey(number.substring(6))) {
+            list = map.get(number.substring(6));
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println("1-" + number.substring(0, 3) + "-" + number.substring(3,6) + "-" + list.get(i).toUpperCase());
+            }
+        } else {
+            System.out.println("1-" + number.substring(0, 3) + "-" + number.substring(3,6) + "-" + number.substring(6));
         }
-        if (map.containsKey(number.substring(3, 6)))
-            // TODO: Finish find words logic
-            return;
     }
 
+    // do our best to not check HashMap, however, there are too many symbols to try to check
     private static boolean isOnlyNumbers(String number) {
         number = number.toLowerCase();
-        if (number.contains("a") || number.contains("b") || number.contains("c") || number.contains("d")
-                || number.contains("e") || number.contains("f") || number.contains("g") || number.contains("h")
-                || number.contains("i") || number.contains("j") || number.contains("k") || number.contains("l")
-                || number.contains("m") || number.contains("n") || number.contains("o") || number.contains("p")
-                || number.contains("q") || number.contains("r") || number.contains("s") || number.contains("t")
-                || number.contains("u") || number.contains("v") || number.contains("w") || number.contains("x")
-                || number.contains("y") || number.contains("z") || number.contains(".") || number.contains("-")
-                || number.contains(",") || number.contains("?") || number.contains("/") || number.contains("\\")
-                || number.contains("$")) {
-            return false;
+        for (int i = 0; i < number.length(); i++) {
+            if (number.charAt(i) != '0' && number.charAt(i) != '1' && number.charAt(i) != '2' && number.charAt(i) != '3'
+                    && number.charAt(i) != '4' && number.charAt(i) != '5' && number.charAt(i) != '6' && number.charAt(i) != '7'
+                    && number.charAt(i) != '8' && number.charAt(i) != '9') {
+                return false;
+            }
         }
         return true;
     }
